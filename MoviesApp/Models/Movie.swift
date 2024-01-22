b88acc2261b76525f3bb1db2e7f6ac6482c63888
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-01-22 18:53:52 -0300

    Build Model and ViewModel

---
+++ MoviesApp/Models/Movie.swift
@@ -0,0 +1,32 @@
+//
+//  Movie.swift
+//  MoviesApp
+//
+//  Created by Jonattan Sousa on 22/01/24.
+//  Copyright © 2024 Jonattan Sousa. All rights reserved.
+//
+
+import Foundation
+
+struct MovieResponse: Codable {
+    let movies: [Movie]
+    
+    private enum CodingKeys: String, CodingKey {
+        case movies = "Search"
+    }
+}
+
+struct Movie: Codable {
+    
+    let title: String
+    let year: String
+    let imdbId: String
+    let poster: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "Title"
+        case year = "Year"
+        case imdbId = "imdbID"
+        case poster = "Poster"
+    }
+}
---
+++ MoviesApp/ViewModels/MovieListViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  MovieListViewModel.swift
+//  MoviesApp
+//
+//  Created by Jonattan Sousa on 22/01/24.
+//  Copyright © 2024 Jonattan Sousa. All rights reserved.
+//
+
+import Foundation
+
+class MovieListViewModel: ObservableObject {
+    
+    @Published var movies = [MovieViewModel]()
+    let httpClient = HTTPClient()
+    
+    func searchByName(_ name: String) {
+        
+        httpClient.getMovies(search: name) { result in
+            switch result {
+            case .success(let movies):
+                if let movies = movies {
+                    DispatchQueue.main.async {
+                        self.movies = movies.map(MovieViewModel.init)
+                    }
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
+    
+}
+
+struct MovieViewModel {
+    
+    let movie: Movie
+    
+    var imdbId: String {
+        movie.imdbId
+    }
+    
+    var title: String {
+        movie.title
+    }
+    
+    var poster: String {
+        movie.poster
+    }
+    
+    var year: String {
+        movie.year
+    }
+    
+}
