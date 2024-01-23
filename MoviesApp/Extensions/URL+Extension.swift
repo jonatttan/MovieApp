277b0d831e5e13d69062dae4555be7088be67431
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-01-23 10:07:03 -0300

    Fix the search address

--- MoviesApp/Extensions/URL+Extension.swift
+++ MoviesApp/Extensions/URL+Extension.swift
@@ -11,7 +11,7 @@ import Foundation
 extension URL {
     
     static func forMovies(_ name: String) -> URL? {
-        URL(string: "http://www.omdbapi.com/?t=\(name)\(Constants.API_KEY)")
+        URL(string: "http://www.omdbapi.com/?s=\(name)\(Constants.API_KEY)")
     }
     
 }
